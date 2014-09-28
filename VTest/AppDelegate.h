aa6729cf5ca4e53d7ae7be917c0fa716a8415975
EdwardDing <nsobject.ding.jiarui@gmail.com>
2014-09-28 17:38:35 +0800

    Initial Commit

---
+++ VTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VTest
+//
+//  Created by JIARUI DING on 9/28/14.
+//  Copyright (c) 2014 JIARUI DING. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VTest/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  VTest
+//
+//  Created by JIARUI DING on 9/28/14.
+//  Copyright (c) 2014 JIARUI DING. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
