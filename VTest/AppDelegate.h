810533c8f1d15e32dab974bbfce7e9e713a3b106
EdwardDing <nsobject.ding.jiarui@gmail.com>
2014-10-04 11:03:38 +0800

    Create

--- VTest/AppDelegate.h
+++ VTest/AppDelegate.h
@@ -7,9 +7,39 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <FMDB/FMDatabase.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
 
+    FMResultSet *randList;
+    FMDatabase *wordsDB;
+    
+    NSMutableArray *buttons;
+    
+    int testScale;
+    int processCount;
+    int index;
+    int correctCount;
+    int correctIndex;
+}
+
+@property (weak) IBOutlet NSTextField *ProgressLabel;
+@property (weak) IBOutlet NSTextField *wordLabel;
+
+@property (weak) IBOutlet NSButton *button1;
+@property (weak) IBOutlet NSButton *button2;
+@property (weak) IBOutlet NSButton *button3;
+@property (weak) IBOutlet NSButton *button4;
+
+
+// IBActions
+- (IBAction)answerSelected:(id)sender;
+- (IBAction)readCurrentWord:(id)sender;
+
+// Supplementary Functions
+-(void)loadNextWord;
+
+void good();
 
 @end
 
--- VTest/AppDelegate.m
+++ VTest/AppDelegate.m
@@ -11,16 +11,90 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    //Initialize
+    testScale = 50;
+    processCount = 0;
+    buttons = [[NSMutableArray alloc]initWithObjects:_button1,_button2,_button3,_button4,nil];
+    
+    // load Data Base
+    NSString *dbPath = [[NSBundle mainBundle] pathForResource:@"wordlist" ofType:@"sqlite"];
+    wordsDB = [[FMDatabase alloc] initWithPath:dbPath];
+    if (![wordsDB open]) {
+        NSLog(@"Open failed, no avaliable resources");
+    }
+    
+    // Execute SQL to create a random wordlist with scale of testScale
+    NSString *sql_randWithScale = [NSString stringWithFormat:@"SELECT * FROM basic_wordlist ORDER BY RANDOM() limit %d", testScale];
+    randList = [wordsDB executeQuery:sql_randWithScale];
+    
+    [self loadNextWord];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+- (IBAction)answerSelected:(id)sender {
+    if ([sender tag] == correctIndex) {
+        correctCount++;
+    }
+    
+    if (processCount < testScale) {
+        [self loadNextWord];
+    } else {
+        NSAlert *alert = [NSAlert alertWithMessageText:@"Finished!"
+                                         defaultButton:@"OK"
+                                       alternateButton:@"Cancel"
+                                           otherButton:nil
+                             informativeTextWithFormat:@"%d Correct!", correctCount];
+        [alert runModal];
+    }
+}
+
+- (void)loadNextWord{
+    
+    // Set the word
+    [randList next];
+    [_wordLabel setStringValue:[randList stringForColumn:@"word"]];
+    
+    
+    // randomly choose a place to put the right answer & fill the other
+    // places with other wrong definitions.
+    correctIndex = arc4random() % 4;
+    FMResultSet *wrongAns = [wordsDB executeQuery:@"select interpret from basic_wordlist order by random() limit 3"];
+    
+    for (int i = 0; i < [buttons count]; i++) {
+        if (i == correctIndex) {
+            [buttons[i] setTitle:[[randList stringForColumn:@"interpret"]
+                                  stringByTrimmingCharactersInSet:
+                                  [NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+        } else {
+            [wrongAns next];
+            [buttons[i] setTitle:[[wrongAns stringForColumn:@"interpret"]
+                                  stringByTrimmingCharactersInSet:
+                                  [NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+        }
+    }
+    
+    processCount++;
+    [_ProgressLabel setStringValue:[NSString stringWithFormat:@"%d",processCount]];
+    
+    // Read the vocabulary
+    NSSpeechSynthesizer *voice = [[NSSpeechSynthesizer alloc] init];
+    [voice startSpeakingString:[randList stringForColumn:@"word"]];
+}
+
+- (IBAction)readCurrentWord:(id)sender {
+    NSSpeechSynthesizer *voice = [[NSSpeechSynthesizer alloc] init];
+    [voice startSpeakingString:[randList stringForColumn:@"word"]];
+}
+
 
 @end
